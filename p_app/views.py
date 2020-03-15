95dd46f7ad70c42b930a8ecf8a94f9da156bcd41
Krishna Venkatramani <kv1g15@soton.ac.uk>
2020-03-15 20:10:48 +0400

    Latest Version🚀

--- p_app/views.py
+++ p_app/views.py
@@ -6,4 +6,4 @@ from .models import work_exp,Project,Description,person,Tech_Framework,Skill_pb
 def index(request):
 	skill_pb = Skill_pb.objects.all()
 	skill_pb = Skill_pb.objects.order_by('-value_percent')
-	return render(request,'p_app/index.html',context={'Person': person.objects.all(),'skill_pbs':skill_pb,'w_exp':work_exp.objects.all(),'tech_frameworks':Tech_Framework.objects.all(),'projects':Project.objects.all()[:4],'projects_2':Project.objects.all()[4:],'description':Description.objects.all()})
+	return render(request,'p_app/index.html',context={'Person': person.objects.all(),'skill_pbs':skill_pb,'w_exp':work_exp.objects.all(),'w_exp_len':len(work_exp.objects.all()),'tech_frameworks':Tech_Framework.objects.all(),'projects':Project.objects.all()[:4],'projects_2':Project.objects.all()[4:],'description':Description.objects.all()})
